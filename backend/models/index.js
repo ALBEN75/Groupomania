2dbb2b65dc75c33e627ee4ef3c677f48b17cb666
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-05-19 23:26:07 +0200

    creation de ma database

---
+++ backend/models/index.js
@@ -0,0 +1,29 @@
+const config = require('../dbConfig');
+const Sequelize = require('sequelize');
+
+const connexion = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+      host: config.HOST,
+      dialect: config.dialect,
+      operatorsAliases: 0,
+  
+      pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle
+      }
+    }
+  );
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.connexion = connexion;
+db.user = require('./usersModel')(connexion, Sequelize);
+
+
+module.exports = db;
