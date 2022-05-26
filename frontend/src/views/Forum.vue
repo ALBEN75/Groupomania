8824ad37de929cb10cc614fdc2726f413ac3d39d
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-05-26 02:56:42 +0200

    ajout page forum

---
+++ frontend/src/views/Forum.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="header">
+        <img alt="icon entreprise groupomania, page forum" src="../assets/icon-one.png" >
+        <nav>
+            <ul>
+                <li>Profil</li> |
+                <li><i class="fa-solid fa-power-off"></i></li>
+            </ul>
+        </nav>    
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+#header{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap; 
+    width: 100%;
+    background: #fe2712;
+    box-shadow: 0px 0px 50px 0px #fe2712;
+    border-radius: 25px; 
+}
+img{
+    width: 50%;
+}
+nav{
+    width: 50%;
+}
+ul{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    list-style-type: none;
+    font-weight: bold;
+}
+li{
+    padding: 0 5%;
+}
+</style>
