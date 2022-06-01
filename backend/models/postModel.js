3ad8baf5caffc4e3abba270d6366d374eca83a30
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-06-01 21:50:10 +0200

    ajout du model pour les posts

---
+++ backend/models/postModel.js
@@ -0,0 +1,42 @@
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+    class Post extends Model {
+        static associate(models) {
+            models.post.belongsTo(models.user, { 
+                onDelete: "CASCADE"
+            })
+        }
+    }
+    Post.init({
+        id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: DataTypes.INTEGER
+		},
+		content: {
+			allowNull: true,
+			type: DataTypes.TEXT
+		},
+		file: {
+			allowNull: true,
+			type: DataTypes.STRING
+		},
+		createdAt: {
+			allowNull: false,
+			type: DataTypes.DATE
+		},
+		updatedAt: {
+			allowNull: false,
+			type: DataTypes.DATE
+		}
+	}, 
+    {
+        //indique a squelize le nom de la table
+        sequelize,
+        modelName: "Post"
+    })
+    return Post
+};
