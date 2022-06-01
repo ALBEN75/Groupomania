b9d23edc9e95c83db0d442e55a31beada2a99a3e
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-06-01 21:48:54 +0200

    ajout des middlewares pour le login et le signup

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -3,15 +3,18 @@ const User = db.user;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res) => {
     User.create({
         username: req.body.username,
+        lastname: req.body.lastname,
+        firstname: req.body.firstname,
+        job: req.body.job,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 8)
     })
     .then(user => {
         res.send({
-            message: "Utilisateur enregistré avec succés",
+            message: "Utilisateur enregistré avec succés"
         })
     })
     .catch(error => {res.status(500).send(error.message)}); 
@@ -20,24 +23,22 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res) => {
     User.findOne({ email: req.body.email })
     .then(user => {
-        if(!user) {
-            return res.status(401).json({ error: "Utilisateur non trouvé !" });
-        }
         bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            if(!valid) {
-                return res.status(401).json({ error: "Mot de passe incorrect !"})
+        .then(verifiedPassword =>{
+            if(!verifiedPassword) {
+                 res.status(400).json({message: "Utilisateur non trouvé!"});
+            } else {
+                res.status(200).json({
+                    user,
+                    token: jwt.sign(
+                        {userId: user.id},
+                        `{process.env.TOKEN}`,
+                        {expiresIn: "24h"}
+                    )
+                })
             }
-            res.status(200).json({ 
-                id: user.id,
-                token: jwt.sign(
-                    { id : user.id },
-                    process.env.TOKEN,
-                    { expiresIn: '24h' }
-                )
-            });
         })
         .catch(error => {res.status(500).send(error.message)}); 
     })
-    .catch(error => {res.status(500).send(error.message)}); 
+    .catch(error => {res.status(500).send(error.message)});
 };
