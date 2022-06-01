8ca48ae222cdfa2888ecc8d40336933edb2416f3
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-06-01 21:49:23 +0200

    modif du middlewares d'authenfication

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.TOKEN);
-    const id = decodedToken.id;
-    
+    console.log(token);
+    //const decodedToken = jwt.verify(token, `${process.env.TOKEN}`);
+    //const id = decodedToken.id;
+    //console.log(decodedToken);
     
     if (req.body.id && req.body.id !== id) {
       throw 'User ID invalide !';
