ccea31f203e5ef134a87239c1a5efb21b9621e10
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-06-01 21:49:48 +0200

    ajout du model pour les commentaires

---
+++ backend/models/commentModel.js
@@ -0,0 +1,45 @@
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+    class Comment extends Model {
+        static associate(models) {
+            models.comment.belongsTo(models.user, { 
+                onDelete: "CASCADE"
+            });
+			models.comment.belongsTo(models.post, { 
+                onDelete: "CASCADE"
+            });
+        }
+    }
+    Comment.init({
+        id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: DataTypes.INTEGER
+		},
+		contentComment: {
+			allowNull: true,
+			type: DataTypes.TEXT
+		},
+		fileComment: {
+			allowNull: true,
+			type: DataTypes.STRING
+		},
+		createdAt: {
+			allowNull: false,
+			type: DataTypes.DATE
+		},
+		updatedAt: {
+			allowNull: false,
+			type: DataTypes.DATE
+		}
+	}, 
+    {
+        //indique a squelize le nom de la table
+        sequelize,
+        modelName: "Comment"
+    })
+    return Comment
+};
